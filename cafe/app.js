8bdd3f7d2d51e782ca8ad1ec38db99b7d2c890d5
user.email <tsb03672@naver.com>
2020-07-04 08:48:30 +0900

    [Graphql] cafe initial commit

---
+++ cafe/app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = { hello: () => 'Hello world!' };
