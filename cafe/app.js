e20ef01aecdda7710a3be776486fde7353424063
user.email <tsb03672@naver.com>
2020-07-04 09:16:04 +0900

    add package & scripts

--- cafe/app.js
+++ cafe/app.js
@@ -3,9 +3,46 @@ var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
 
 var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
+    type Query {
+        hello: String
+        coffee: [Coffee]
+        users: [User]
+    }
+
+    type Coffee {
+        id: Int,
+        name: String,
+        price: Int,
+        stars: Int,
+    }
+
+    type User {
+        id: Int,
+        name: String,
+    }
 `);
 
-var root = { hello: () => 'Hello world!' };
+var root = {
+    hello: () => 'hello world!',
+    users: () => {
+        return [
+            { id: 1, name: "Jake" },
+            { id: 2, name: "Park" },
+            { id: 3, name: "Selena" }
+        ]
+    },
+    coffee: () => {
+        return [
+            { id: 1, name: "Americano", price: 2500 },
+        ]
+    }
+}
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,    
+    graphiql: true,
+}));
+
+app.listen(4000, () => console.log('Now broswe to localhost:4000/graphql'));
