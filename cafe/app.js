47738f4f765f02653e5d8e05b4b1af5a8abac8f2
user.email <tsb03672@naver.com>
2020-07-05 10:14:17 +0900

    addd ssr in nodejs

--- cafe/app.js
+++ cafe/app.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+const queryString = require('query-string');
 
 const model = require('./mongodb/mongo.js');
 var schema = buildSchema(`
@@ -53,6 +54,38 @@ app.use('/graphql', graphqlHTTP({
     rootValue: root,    
     graphiql: true,
 }));
+
+app.get('/login/facebook', function(req, res) {
+
+    const stringifiedParams = queryString.stringify({
+        client_id: "331044621072978",
+        redirect_uri: 'http://localhost:4000/auth/callback',
+        scope: ['email', 'user_friends'].join(','), // csv format
+        response_type: 'code',
+        auth_type: 'rerequest',
+        display: 'popup',
+    });
+
+    res.type('text/html').status(200).send(`
+        <!DOCTYPE html>
+        <html>
+            <body>
+            <a href="https://www.facebook.com/v4.0/dialog/oauth
+            ?auth_type=rerequest
+            &client_id=331044621072978
+            &display=popup
+            &redirect_uri=http%3A%2F%2Flocalhost%3A4000%2Fauth%2Fcallback
+            &response_type=code&scope=email%2Cuser_friends">
+                Login with Facebook
+            </a>
+            </body>
+        </html>
+    `);
+});
+
+app.get('/auth/callback', function(req, res) {
+    res.send("authentification success");
+});
 
 app.get('*', function(req, res){
     res.end('Hello world');
