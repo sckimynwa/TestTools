d815d7208b50a7b0aa6d72542ac6e7d84fa848fc
user.email <tsb03672@naver.com>
2020-07-12 21:38:46 +0900

    kakao auth setting

--- notifier/kakao.ts
+++ notifier/kakao.ts
@@ -1,28 +1,49 @@
 import axios from "axios";
 
-const token = "WlrhcU4Y_fv3NNvGcWd-YIE6dcmc_ojXC0AL5go9cpcAAAFzPNsYuA";
-const options = {
-  headers: {
-    Authorization: `Bearer ${token}`
-  }
+// Auth Settings
+const token = "pHUVaON2FjzogE9rRLU-L5OhECFUBJn79JIsRAo9dNkAAAFzQwLIcg";
+const headers = {
+  Authorization: `Bearer ${token}`,
+  "content-type": "application/x-www-form-urlencoded"
 };
 
 // Get User Information
 async function getMe() {
-  const data = await axios.get("https://kapi.kakao.com/v2/user/me", options);
+  const data = await axios({
+    method: "GET",
+    url: "https://kapi.kakao.com/v2/user/me",
+    headers
+  });
   return data;
 }
 
 // post Message for Myself
 async function postMessageForMe() {
-  const data = await axios.post(
-    "https://kapi.kakao.com/v2/api/talk/memo/default/send",
-    {
-      template_id: 32355
-    },
-    options
-  );
+  const data = await axios({
+    method: "POST",
+    url: "https://kapi.kakao.com/v2/api/talk/memo/default/send",
+    headers,
+    data: {
+      template_object: {
+        object_type: "text",
+        text: "sample text",
+        link: {
+          web_url: "https://www.naver.com",
+          mobile_web_url: "https://www.naver.com"
+        },
+        button_title: "sample button"
+      }
+    }
+  });
   return data;
 }
 
-postMessageForMe().then(res => console.log(res));
+// getMe().then(res => console.log(res.data));
+postMessageForMe().then(
+  res => {
+    console.log(res);
+  },
+  err => {
+    console.log(err);
+  }
+);
